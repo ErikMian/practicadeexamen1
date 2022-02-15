39c98db56182115f94e6bdb36e60b4b8c2636be2
ErikMian <80875212+ErikMian@users.noreply.github.com>
2022-02-15 12:23:00 -0600

    examen primer parcial paginas web

---
+++ erikmian.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Erik Mian</title>
+
+  <style>
+    table {
+   width: 30%;
+   border: 1px solid #000000;
+}
+th, td {
+   width: 25%;
+   text-align: center;
+   vertical-align: top;
+   border: 1px solid #000000;
+
+}
+
+
+
+  </style>
+</head>
+<body>
+
+<h1>Examen de Programación de sitios web  Erik Mian</h1>
+
+<?php
+echo "
+
+
+<table >
+<tr>
+
+<th rowspan='4'>México</th>
+
+  <td >Refoma</td>
+
+  <td><a href='https://www.reforma.com/'>www.reforma.com</a></td>
+
+</tr>
+
+<tr>
+
+  <td>El Universal</td>
+
+  <td><a href='http://www.eluniversal.com.mx/'>www.eluniversal.com.mx</a></td>
+
+</tr>
+
+<tr>
+
+  <td>El Financiero</td>
+
+  <td><a href='https://www.elfinanciero.com.mx/'>www.elfinanciero.com.mx</a></td>
+
+</tr>
+
+<tr>
+
+  <td>PorEsto</td>
+
+  <td><a href='https://www.poresto.net/'>www.poresto.net</a></td>
+
+</tr>
+
+
+
+</table>
+<br>
+<br>
+
+<table >
+
+<tr>
+
+<center><th rowspan='3'>España</th></center>
+
+  <td >El Comercio</td>
+
+  <td><a href='https://www.elcomercio.es/'>www.elcomercio.es</a></td>
+
+</tr>
+
+<tr>
+
+  <td>20 Minutos</td>
+
+  <td><a href='https://www.20minutos.es/'>www.20minutos.es</a></td>
+
+</tr>
+
+<tr>
+
+  <td>El Diario</td>
+
+  <td><a href='https://www.eldiario.es/'>www.eldiario.es</a></td>
+
+</tr>
+</table>
+
+<br>
+<br>
+<table >
+<tr>
+
+<center><th rowspan='2'>Francia</th></center>
+
+  <td >Libération</td>
+
+  <td><a href='https://www.liberation.fr/'>www.liberation.fr</a></td>
+
+</tr>
+
+<tr>
+
+  <td>Sud Ouest</td>
+
+  <td><a href='https://www.sudouest.fr/'>www.sudouest.fr</a></td>
+
+</tr>
+</table>
+
+
+"
+
+
+?>
+</body>
+</html>
+
